6247b54ee372c5cd602c7081c3a103531d9edc87
Dennis Dang <dangggdennis@gmail.com>
2023-03-02 13:51:33 -0500

    attempt 3b

--- 3-broadcast/main.go
+++ 3-broadcast/main.go
@@ -7,49 +7,85 @@ import (
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 )
 
+type BroadcastBody = struct {
+	Message int32  `json:"message"`
+	Type    string `json:"type"`
+}
+
+func retrySend(node *maelstrom.Node, msg int32, nodeId string) {
+	body := BroadcastBody{
+		Type:    "broadcast",
+		Message: msg,
+	}
+
+	err := node.Send(nodeId, body)
+	if err != nil {
+		retrySend(node, msg, nodeId)
+	}
+}
+
 func main() {
 	n := maelstrom.NewNode()
 
-	type b1 = struct {
-		Message any `json:"message"`
-	}
-
-	store := []any{}
+	neighbors := []string{}
+	store := map[int32]int32{}
 
 	n.Handle("broadcast", func(msg maelstrom.Message) error {
-		var body b1
+
+		var body BroadcastBody
 		if err := json.Unmarshal(msg.Body, &body); err != nil {
 			return err
 		}
 
-		resp := map[string]any{}
+		_, exists := store[body.Message]
+		if !exists {
+			store[body.Message] = body.Message
+		}
+
+		for _, node := range neighbors {
+			go retrySend(n, body.Message, node)
+		}
+
+		resp := map[string]string{}
 		resp["type"] = "broadcast_ok"
-		store = append(store, body.Message)
 
 		return n.Reply(msg, resp)
 	})
 
 	n.Handle("read", func(msg maelstrom.Message) error {
-		var body map[string]any
+		resp := map[string]any{}
+		resp["type"] = "read_ok"
+		msgs := []int32{}
+		for _, v := range store {
+			msgs = append(msgs, v)
+		}
+		resp["messages"] = msgs
+
+		return n.Reply(msg, resp)
+	})
+
+	n.Handle("topology", func(msg maelstrom.Message) error {
+		type Topology = map[string][]string
+
+		type TopologyBody = struct {
+			Type     string   `json:"type"`
+			Topology Topology `json:"topology"`
+		}
+
+		var body TopologyBody
 		if err := json.Unmarshal(msg.Body, &body); err != nil {
 			return err
 		}
 
-		body["type"] = "read_ok"
-		body["messages"] = store
+		topology, exists := body.Topology[n.ID()]
+		if exists {
+			neighbors = append(neighbors, topology...)
+		}
 
-		return n.Reply(msg, body)
-	})
+		resp := map[string]string{}
+		resp["type"] = "topology_ok"
 
-	n.Handle("topology", func(msg maelstrom.Message) error {
-		body := map[string]any{}
-		// if err := json.Unmarshal(msg.Body, &body); err != nil {
-		// 	return err
-		// }
-
-		body["type"] = "topology_ok"
-
-		return n.Reply(msg, body)
+		return n.Reply(msg, resp)
 	})
 
 	if err := n.Run(); err != nil {
--- readme.md
+++ readme.md
@@ -6,4 +6,6 @@
 
 3. challenge 2 unique id generation: `./maelstrom test -w unique-ids --bin ~/go/bin/maelstrom-unique-ids --time-limit 30 --rate 1000 --node-count 3 --availability total --nemesis partition`
 
-4. challenge 3 broadcast: `./maelstrom test -w broadcast --bin ~/go/bin/maelstrom-broadcast --node-count 1 --time-limit 20 --rate 10`
+4. challenge 3a broadcast: `./maelstrom test -w broadcast --bin ~/go/bin/maelstrom-broadcast --node-count 1 --time-limit 20 --rate 10`
+
+5. challenge 3b broadcast: `./maelstrom test -w broadcast --bin ~/go/bin/maelstrom-broadcast --node-count 5 --time-limit 20 --rate 10`
